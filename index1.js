8cbd7389c627dbb1fc8a645931a2c078463bc61c
alaaz2006 <alaazarhaimanot@gmail.com>
2024-11-10 20:26:18 +0300

    Add a paragraph

--- index1.js
+++ index1.js
@@ -1,13 +1,17 @@
 import express from "express";
 import superheroes from "superheroes";
 
+
 const app = express();
 const port = 3000;
 
+// app.use("view engine","ejs")
+// app.use(express.static("public"))
 
 app.get("/", (req, res) => {
   let name = superheroes.random();
-  res.send(`<h1>Behold, I am ${name}!</h1>`);
+  res.send(`<h1>Behold, I am ${name}!</h1> <hr> <p> 💚This is my first paragraph to the project✋ </p>`);
+  
 });
 
 app.listen(port, () => {
